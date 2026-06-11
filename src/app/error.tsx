5c0daeedd7b60ae8eda189fe244aa64d0f048c21
vtnkania <itskavacode@gmail.com>
2026-06-11 19:53:29 +0700

    first commit

---
+++ src/app/error.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  return (
+    <div>
+      <h2>Something went wrong!</h2>
+
+      <button onClick={() => reset()}>
+        Try Again
+      </button>
+    </div>
+  );
+}
